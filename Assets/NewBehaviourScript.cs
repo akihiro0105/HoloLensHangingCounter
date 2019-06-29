8e498f45b12c4eba85c82d856f87709070f584a7
akihiro <ueyama0105@gmail.com>
2019-06-29 16:51:54 +0900

    first commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    [SerializeField] private Text countText;
+
+    private int count = 0;
+	// Use this for initialization
+	void Start () {
+        countText.text = count.ToString();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        count++;
+        countText.text = count.ToString();
+    }
+}
